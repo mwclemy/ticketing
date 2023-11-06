7a024cde9ae4a50ace7c85709b438e69e64076a7
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2023-11-05 21:03:49 -0700

    added signup tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('fails when unexisting email is supplied', async () => {
+    await request(app).post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400)
+})
+
+it('fails when an invalid password is supplied', async () => {
+    await request(app).post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    await request(app).post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'gjgjgjgjg'
+        })
+        .expect(400)
+})
+
+it('sets a cookie upon successful signin', async () => {
+    await request(app).post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    const response = await request(app).post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200)
+
+    expect(response.get('Set-Cookie')).toBeDefined()
+
+})
