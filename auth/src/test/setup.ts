c8e0b6e3e83e35d53e48280fa80f5290f88d66d1
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2023-11-05 22:23:34 -0700

    completed writting tests for the auth service

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,14 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
 
 let mongo: any
+
+declare global {
+    function signin(): Promise<string[]>
+}
+
 beforeAll(async () => {
     process.env.JWT_KEY = 'asdfasdf';
 
@@ -25,3 +31,16 @@ afterAll(async () => {
     await mongo.stop()
     await mongoose.connection.close()
 })
+
+global.signin = async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201)
+
+    const cookie = response.get('Set-Cookie')
+    return cookie
+}
